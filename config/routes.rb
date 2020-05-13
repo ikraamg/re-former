6519457ee7f51e49260efd7e83e98d85b03e2bb5
Ikraam <ikraamghoor@gmail.com>
2020-05-13 13:22:02 +0200

    completed until form_for edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
+# frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: %i[new create]
+  resources :users
 end
